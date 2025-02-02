fcb5869f49ee82e1c2130fa61be212d3c3ce636a
Ashton Pate <arpate@amarillocollege.com>
2025-02-02 01:29:42 -0800

    Add project files.

---
+++ Wk3Ex3/Program.cs
@@ -0,0 +1,36 @@
+﻿//declares variable for user input
+string userinputString;
+
+//declares variable for what operation the user wants
+int userinputOperation;
+
+//promts askign for user for a string variable
+Console.WriteLine("please input a string variable ");
+
+//collects user input for the string variable
+userinputString = Console.ReadLine();
+
+//promts user to select which opertaiton they want
+Console.WriteLine(" please select what operation do you want ");
+Console.WriteLine(" ReverseString enter 1 ");
+Console.WriteLine(" CountVowels enter 2 ");
+Console.WriteLine(" IsPalindrome enter 3 ");
+
+//collects user input for the operation
+userinputOperation = Convert.ToInt32(Console.ReadLine());
+
+//calutes opertaion for reversestring
+if (userinputOperation == 1)
+{
+
+}
+//calutes opertaion for countvowels
+else if (userinputOperation == 2)
+{
+
+}
+//calutes opertaion for palindrome
+else if (userinputOperation == 3)
+{
+
+}
