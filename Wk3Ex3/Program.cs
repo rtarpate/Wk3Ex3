b26b896fdfe74003558f09428bf3e323ae3f7519
Ashton Pate <arpate@amarillocollege.com>
2025-02-02 02:06:50 -0800

    palindrome operation works

--- Wk3Ex3/Program.cs
+++ Wk3Ex3/Program.cs
@@ -4,13 +4,20 @@ string userinputString;
 //decalres variable for reversed user input
 string userReverse;
 
+//decalres variable for palindrome user input
+string userpalindrome;
 
+//decalres varible for vowel count
+int vowelcount;
 
 //declares variable for what operation the user wants
 int userinputOperation;
 
-//declares variable for an array
-char[] userArray;
+//declares variable for an array for reverser operation
+char[] userArrayReverse;
+
+//declares variable for an array for reverser operation
+char[] userArrayPalindrome;
 
 //promts askign for user for a string variable
 Console.WriteLine("please input a string variable ");
@@ -31,15 +38,17 @@ userinputOperation = Convert.ToInt32(Console.ReadLine());
 if (userinputOperation == 1)
 {
     //converts user input form string into a array
-    userArray = userinputString.ToCharArray();
+    userArrayReverse = userinputString.ToCharArray();
 
     //reverees the array
-    Array.Reverse(userArray);
+    Array.Reverse(userArrayReverse);
 
     //converts the array back into string
-    userReverse = new string(userArray);
+    userReverse = new string(userArrayReverse);
 
+    //outputs the results of reveresing the user input
     Console.WriteLine(" the reversed of the user input " + userReverse);
+    Console.ReadLine();
 
 }
 //calutes opertaion for countvowels
@@ -50,5 +59,28 @@ else if (userinputOperation == 2)
 //calutes opertaion for palindrome
 else if (userinputOperation == 3)
 {
-    //
+    //converts user input form string into a array
+    userArrayPalindrome = userinputString.ToCharArray();
+
+    //reverees the array
+    Array.Reverse(userArrayPalindrome);
+
+    //converts the array back into string
+    userpalindrome = new string(userArrayPalindrome);
+
+    if (userpalindrome == userinputString)
+    {
+        //prints out the results if user input is a palindrome
+        Console.WriteLine(" user input was a palindrome ");
+        Console.WriteLine(userpalindrome + " = " + userinputString);
+
+    }
+    else if (userpalindrome != userinputString)
+    {
+        //prints out the results if the user input is not a palindrome
+        Console.WriteLine(" user input was not a palindrome ");
+        Console.WriteLine(userinputString + " revesred is  " + userpalindrome);
+    }
+
+    
 }
